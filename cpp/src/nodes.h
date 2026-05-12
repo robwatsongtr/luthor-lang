895fbfc6167db0afc113aba666253c470f09d700
Rob Watson <rwatso@gmail.com>
2026-05-12 14:46:40 -0700

    added some nodes

--- cpp/src/nodes.h
+++ cpp/src/nodes.h
@@ -20,5 +20,32 @@ struct UnaryOpNode : public ASTNode {
         : op_type(op_type), operand(std::move(operand)) {}
 };
 
+struct BinaryOpNode : public ASTNode {
+    TokenType op_type;
+    std::unique_ptr<ASTNode> left;
+    std::unique_ptr<ASTNode> right;
 
+    BinaryOpNode(TokenType op_type, std::unique_ptr<ASTNode> left, 
+          std::unique_ptr<ASTNode> right) 
+        : op_type(op_type), left(std::move(left)), right(std::move(right)) {}
+};
 
+struct NumberNode : public ASTNode {
+    double number; 
+
+    NumberNode(double number) : number(number) {}
+}; 
+
+struct IdentifierNode : public ASTNode {
+    std::string identifier; 
+
+    IdentifierNode(std::string identifier) : identifier(identifier) {}
+};
+
+struct AssignNode : public ASTNode {
+    std::string var_name;
+    std::unique_ptr<ASTNode> expression;
+
+    AssignNode(std::string var_name, std::unique_ptr<ASTNode> expression)
+        : var_name(var_name), expression(std::move(expression)) {}
+};
